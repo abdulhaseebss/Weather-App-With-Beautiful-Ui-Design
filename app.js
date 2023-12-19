cb2b31785f30f1eaa3f1a0ce35d655d2e15960a3
abdulhaseebss <m.saleemhaseeb@gmail.com>
2023-12-19 14:46:38 +0500

    Weather App Compelete

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const div = document.querySelector('.div-centr');
 
 div.style.display = 'none'
 
+
 form.addEventListener('submit' , function(e){
     e.preventDefault(); 
     axios.get(`https://api.weatherapi.com/v1/current.json?key=27c33b52b37744b28d1164726231710&q=${input.value}`)
@@ -17,11 +18,12 @@ form.addEventListener('submit' , function(e){
         div.innerHTML = `<div class = "flex-div"><div class="main-div-2">
         <div class="main-div d-flex justify-content-between">
             <div>
-            <h1 class="animate__animated animate__bounce">hello World</h1>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <h1 class="mt-5  animate__animated animate__pulse">30</h1>
+            <h1 class="animate__animated animate__bounce">${data.location.name}</h1>
+            <p>${data.location.tz_id}</p>
+            <p>${data.location.country}</p>
+            <h1 class="mt-5  animate__animated animate__pulse">${data.current.temp_c}<sup>°C</sup></h1>
             </div>
-            <img class="animate__animated animate__fadeInDown" src="https://cdn.weatherapi.com/weather/64x64/day/116.png" width="100px" height="100px" alt="">
+            <img class="animate__animated animate__fadeInDown" src="${data.current.condition.icon}" width="100px" height="100px" alt="">
         </div>
         <div class="main-div mt-4">
             <h5 class="fs-6 mb-5">Air Conditions</h5>
@@ -29,17 +31,17 @@ form.addEventListener('submit' , function(e){
     
             <div class="new-flex">
                 <h5><i class="fa-solid fa-temperature-three-quarters fa-shake"></i>&nbsp; Real Feel</h5>
-                <h1 class="condition animate__animated animate__fadeInLeft">30</h1>
+                <h1 class="condition animate__animated animate__fadeInLeft">${data.current.feelslike_c}<sup>°C</sup></h1>
                 <br><br>
                 <h5><i class="fa-solid fa-droplet fa-bounce"></i>&nbsp; Humidity</h5>
-                <h1 class="condition animate__animated animate__fadeInLeft">30</h1>
+                <h1 class="condition animate__animated animate__fadeInLeft">${data.current.humidity} %</h1>
             </div>
             <div class="new-flex">
                 <h5><i class="fa-solid fa-wind fa-fade"></i>&nbsp; Wind</h5>
-                <h1 class="condition animate__animated animate__fadeInRight">30</h1>
+                <h1 class="condition animate__animated animate__fadeInRight">${data.current.wind_kph} km/h</h1>
                 <br><br>
                 <h5><i class="fa-solid fa-sun fa-spin"></i>&nbsp; UV Index</h5>
-                <h1 class="condition animate__animated animate__fadeInRight">30</h1>
+                <h1 class="condition animate__animated animate__fadeInRight">${data.current.uv}</h1>
             </div>
             </div>
         </div>
